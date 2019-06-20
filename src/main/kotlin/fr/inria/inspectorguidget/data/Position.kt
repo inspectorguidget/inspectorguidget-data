38feb11043601ceb529f388bf82bbfdbf66be942
arnobl <1052622+arnobl@users.noreply.github.com>
2019-06-20 20:28:06 +0200

    config(mvn): maven configuration plus first files

---
+++ src/main/kotlin/fr/inria/inspectorguidget/data/Position.kt
@@ -0,0 +1,3 @@
+package fr.inria.inspectorguidget.data
+
+data class Position(val file: String, val start: Int, val end: Int)
---
+++ src/main/kotlin/fr/inria/inspectorguidget/data/UICommand.kt
@@ -0,0 +1,3 @@
+package fr.inria.inspectorguidget.data
+
+data class UICommand(val name: String, val position: Position)
