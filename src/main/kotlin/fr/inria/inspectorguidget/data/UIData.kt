1e5821693f30a155c2e807a3d9a50983cc7ad346
arnobl <1052622+arnobl@users.noreply.github.com>
2019-07-04 13:16:33 +0200

    new(model): uidata class

--- src/main/kotlin/fr/inria/inspectorguidget/data/UIData.kt
+++ src/main/kotlin/fr/inria/inspectorguidget/data/UIData.kt
@@ -1,2 +1,17 @@
 package fr.inria.inspectorguidget.data
 
+import java.util.stream.Collectors
+
+data class UIData(val widgetBindings: List<WidgetBinding>) {
+    fun getInteractions(): Set<Interaction> =
+        widgetBindings
+            .stream()
+            .map { wb -> wb.interaction }
+            .collect(Collectors.toSet())
+
+    fun getCommands(): Set<UICommand> =
+        widgetBindings
+            .stream()
+            .map { wb -> wb.cmd }
+            .collect(Collectors.toSet())
+}
